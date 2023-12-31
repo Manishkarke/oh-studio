c114827bb4eb32cc0498075942a23d3d90176931
Manishkarke <mk4345437@gmail.com>
2024-01-01 04:28:43 +0545

    Completed responsive header section

---
+++ src/assets/Icon.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Icon() {
+  return (
+    <svg
+      className="snow-icon"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      viewBox="0 0 12 12"
+    >
+      <path
+        d="M 6 0 L 6 12 M 0 6 L 12 6 M 1.757 1.758 L 10.243 10.242 M 1.758 10.243 L 10.242 1.757"
+        fill="transparent"
+        strokeWidth="1.45"
+        strokeMiterlimit="10"
+      ></path>
+    </svg>
+  );
+}
+
+export default Icon;
--- src/components/header/Hero.jsx
+++ src/components/header/Hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.css";
+import Icon from "../../assets/Icon";
 
 function Hero() {
   return (
@@ -11,8 +12,18 @@ function Hero() {
               a brand and product designer working with clients globally
             </h1>
           </div>
-          <div className="hero-tags-container"></div>
+          <div className="hero-tags-container">
+            <p className="hero-tag">expertise</p>
+            <p className="hero-tag">branding</p>
+            <p className="hero-tag">product</p>
+            <p className="hero-tag">design systems</p>
+          </div>
         </section>
+
+        <button className="btn sticky">
+          <Icon /> 
+          <span>shop</span>
+        </button>
       </header>
     </>
   );
